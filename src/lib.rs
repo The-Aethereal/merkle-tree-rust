51ae18a1fceb6ec4a265776f297d8da5c9871ec7
The-Aethereal <suryanshctf05@gmail.com>
2026-01-15 11:25:09 +0000

    transaction hashing

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use alloy_primitives::{B256, keccak256};
+
+pub fn hash_txn(txn : String) -> B256{
+    return keccak256(txn);
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn main(){
+    let txns : Vec<String> = ["txn1", "txn2", "txn3"].iter()
+        .map(|str| str.to_string())
+        .collect();
+    
 }
